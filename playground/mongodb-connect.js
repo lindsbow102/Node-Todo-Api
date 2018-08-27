aa675fbe766376fe53c732b717269831ec67e4ba
Lindsey Bowen <lindsbow102@yahoo.com>
2018-08-26 19:22:47 -0700

    Object ID / Find data playgrounds

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+const obj = new ObjectID();
+console.log(obj);
+
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
     if (err) {
@@ -17,16 +23,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     //     console.log(JSON.stringify(result.ops, undefined, 2)); // ops attribute will store all of the docs that were inserted
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Lindsey Bowen',
-        age: 38,
-        location: 'Gilbert, AZ'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert User', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    })
+    // db.collection('Users').insertOne({
+    //     name: 'Lindsey Bowen',
+    //     age: 38,
+    //     location: 'Gilbert, AZ'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert User', err);
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
 
-    client.close(); // Closes connection with MongoDB server
+    // client.close(); // Closes connection with MongoDB server
 });
