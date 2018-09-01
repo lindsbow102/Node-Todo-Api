67d46341189313aab8a02f5055b5f0dd2a09cfd1
Lindsey Bowen <lindsbow102@yahoo.com>
2018-09-01 15:21:25 -0700

    Validators, types and defaults

--- server/server.js
+++ server/server.js
@@ -5,39 +5,61 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,  // Prevents mongoDB from accepting an empty string
+        trim: true //Cuts out any white space before or after text
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 // Use constructor function to declare new todo
-const newTodo = new Todo({
-    text: 'Pay Band fees',
-    completed: true,
-    completedAt: 20
+// const newTodo = new Todo({
+//     text: 'Pay Band fees',
+//     completed: true,
+//     completedAt: 20
+// });
+
+// // Need to call save in order to actually send data to mongo
+// newTodo.save().then((doc) => {
+//     console.log(`Saved Todo: ${doc}`);
+// }, (e) => {
+//     console.log('Unable to save Todo');
+// });
+
+// const otherTodo = new Todo({
+//     text: ' Edit this video '
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log(`Saved other todo: ${doc}`);
+// }, (e) => {
+//     console.log('Unable to save other todo');
+// });
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        minLength: 1
+    }
 });
 
-// Need to call save in order to actually send data to mongo
-newTodo.save().then((doc) => {
-    console.log(`Saved Todo: ${doc}`);
-}, (e) => {
-    console.log('Unable to save Todo');
+const newUser = new User({
+    email: 'lindsey@test.com'
 });
 
-const otherTodo = new Todo({
-    text: 'Get flu shot',
-    completed: true,
-    completedAt: 23000
-});
-
-otherTodo.save().then((doc) => {
-    console.log(`Saved other todo: ${doc}`);
-}, (e) => {
-    console.log('Unable to save other todo');
+newUser.save().then((doc) => {
+    console.log(`Saved new user: ${doc}`);
+}, e => {
+    console.log('Unable to save User', e);
 });
 
