71416b226089d76edf092e0d495dda497953c968
Lindsey Bowen <lindsbow102@yahoo.com>
2018-09-13 14:23:47 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -26,6 +27,25 @@ app.get('/todos', (req, res) => {
         res.send({ todos }); // Using an object to set us up for a more flexible future
     }, e => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/12341234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } 
+
+        res.send({ todo });  // Using an object gives us more options down the line, like adding properties
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
