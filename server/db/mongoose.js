74abf7039158d9ef72653f17d770ecd51f190ec3
Lindsey Bowen <lindsbow102@yahoo.com>
2018-10-13 10:50:21 -0700

    Test database created

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  // We want to use the mongoose promise library even though it supports callbacks
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = { mongoose };
