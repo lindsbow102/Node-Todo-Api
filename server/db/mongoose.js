62c8716439c48342d1ec64cf477fe2799349adc3
Lindsey Bowen <lindsbow102@yahoo.com>
2018-10-15 14:30:01 -0700

    Set up User model and POST request

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  // We want to use the mongoose promise library even though it supports callbacks
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 module.exports = { mongoose };
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+// UserAuth = {
+//     email: 'lindsey@fakemail.com',
+//     password: 'ldkjsjdidjdksldidkke',
+//     tokens: [{
+//         access: 'auth',
+//         token: 'aksjdkflidjskdlpiekdjs'
+//     }]
+// }
 
 const User = mongoose.model('User', {
     email: {
         type: String,
-        require: true,
+        required: true,
         trim: true,
-        minLength: 1
-    }
+        minLength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = { User };
