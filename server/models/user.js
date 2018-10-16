f07e808ac72b4916c04988f8cd087320895b7455
Lindsey Bowen <lindsbow102@yahoo.com>
2018-10-16 11:20:42 -0700

    Generate auth tokens / Setting headers

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// UserAuth = {
-//     email: 'lindsey@fakemail.com',
-//     password: 'ldkjsjdidjdksldidkke',
-//     tokens: [{
-//         access: 'auth',
-//         token: 'aksjdkflidjskdlpiekdjs'
-//     }]
-// }
-
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -38,5 +31,30 @@ const User = mongoose.model('User', {
         }
     }]
 });
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject  = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']); 
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    const user = this; // Arrow functions do not bind "this".  Must use regular functions
+    const access = 'auth';
+    const token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, 'abc123').toString();
+
+    //user.tokens.concat([{access, token}]);
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+}
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
